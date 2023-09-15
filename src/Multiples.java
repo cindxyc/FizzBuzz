b1c4ba7a9681d141a921a00c76d6fa7e6230fcb6
cindxyc <cindxyc@cindxycs-MacBook-Pro.local>
2023-09-15 19:17:07 -0400

    refactored to extract helper method

---
+++ src/Multiples.java
@@ -0,0 +1,19 @@
+public class Multiples {
+    public static void main(String[] args) {
+
+        int count = 0;
+        for (int i = 1; i <= 1000; i++) {
+
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+
+                count++;
+
+            }
+        }
+        System.out.println(count);
+    }
+}
