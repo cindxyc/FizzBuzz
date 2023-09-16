6640b1b656db4f5feadf7cc8b3f38a31663a744a
cindxyc <cindxyc@cindxycs-MacBook-Pro.local>
2023-09-15 20:03:15 -0400

    refactored to extract helper method

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+
+        int count = 0;
+        int i = 100;
+        while (i > 0) {
+
+        // Find out which numbers divide i.
+        boolean even = i % 2 == 0;
+
+        if (even) {
+
+            i /= 2;
+
+        } else {
+
+            i -= 1;
+
+        }
+            count++;
+        }
+        System.out.println(count);
+    }
 }
